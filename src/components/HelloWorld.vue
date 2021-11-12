143b940ec75cb15beea4adcf6baf41d649f133d2
Bja-ja <b17611596313@163.com>
2021-11-12 20:26:56 +0800

    sty

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,16 @@
 		    <el-table-column prop="name" label="Name" width="180" />
 		    <el-table-column prop="address" label="Address" />
 		  </el-table>
+      <div class="block">
+          <span class="demonstration"
+            >Switch when indicator is hovered (default)</span
+          >
+          <el-carousel height="150px">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3 class="small">{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
 	</div>
 </template>
 
@@ -54,5 +64,21 @@ li {
 }
 a {
   color: #42b983;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
